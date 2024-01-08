5dad041c2891b80f0b2549c58d296216290bee11
madison24 <mhousman24@gmail.com>
2024-01-08 14:02:10 -0600

    4.5

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -5,6 +5,10 @@ const CitySearch = ({ allLocations, setCurrentCity }) => {
   const [query, setQuery] = useState("");
   const [suggestions, setSuggestions] = useState([]);
 
+  useEffect(() => {
+    setSuggestions(allLocations);
+  }, [`${allLocations}`]);
+
   const handleInputChange = (event) => {
     const value = event.target.value;
     const filteredLocations = allLocations
@@ -22,10 +26,6 @@ const CitySearch = ({ allLocations, setCurrentCity }) => {
     setShowSuggestions(false);
     setCurrentCity(value);
   };
-
-  useEffect(() => {
-    setSuggestions(allLocations);
-  }, [`${allLocations}`]);
 
   return (
     <div id="city-search">
