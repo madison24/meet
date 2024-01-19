6eef7ed034f86e060043dab871581d42339f69b8
madison24 <mhousman24@gmail.com>
2024-01-19 13:29:49 -0600

    4.8

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -42,8 +42,8 @@ class InfoAlert extends Alert {
 class ErrorAlert extends Alert {
   constructor(props) {
     super(props);
-    this.color = "rgb(204, 43, 10)"; // red
-    this.bgColor = "rgb(252, 170, 149)"; // light red
+    this.color = "rgb(255, 0, 0)"; // red
+    this.bgColor = "rgb(255, 204, 209)"; // light red
   }
 }
 
